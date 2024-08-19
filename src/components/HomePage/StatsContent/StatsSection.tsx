633b9139058bafa657890a2c8436b4964e115843
Hasnat Ahmed Goheer <ch.hsyahmedgoheer@hotmail.com>
2024-08-19 16:18:25 +0500

    adjust the statsSection with proper margin

--- src/components/HomePage/StatsContent/StatsSection.tsx
+++ src/components/HomePage/StatsContent/StatsSection.tsx
@@ -2,20 +2,35 @@
 import SlideWrapper from "@/components/Wrapper/SlideWrapper";
 import homepageContent from "@/content/homepage";
 import theme from "@/utils/theme";
-import { Container, Divider, Grid, Link, Typography, useMediaQuery } from "@mui/material";
+import {
+  Container,
+  Divider,
+  Grid,
+  Link,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
 
 const StatsSection = () => {
-  const isAboveMd = useMediaQuery(theme.breakpoints.up('md'));
+  const isAboveMd = useMediaQuery(theme.breakpoints.up("md"));
 
   return (
-    <Container maxWidth="md" className={isAboveMd ? "" : '!px-6 md:!px-8'} sx={{ my: 15  }}>
+    <Container
+      maxWidth="md"
+      className={isAboveMd ? "" : "!px-6 md:!px-8"}
+      sx={{ my: 15 }}
+    >
       <SlideWrapper>
         <Grid container spacing={6} alignItems="center">
           <Grid item xs={12} md={6}>
             <Typography variant="h4" color="text.primary" gutterBottom>
               {homepageContent.statsContentMainTitle}
             </Typography>
-            <Typography variant="body1" color="background.lightAshGrey" paragraph>
+            <Typography
+              variant="body1"
+              color="background.lightAshGrey"
+              paragraph
+            >
               {homepageContent.statsContentDescription}
             </Typography>
             <Link
@@ -34,17 +49,21 @@ const StatsSection = () => {
               flexItem
               sx={{
                 position: "absolute",
-                left: 0,
+                left: 10,
                 top: 10,
                 bottom: 0,
                 bgcolor: "text.primary",
                 width: "2px",
               }}
             />
-            <Grid container spacing={4} sx={{ marginLeft: "20px" }}>
+            <Grid container spacing={4} sx={{ marginLeft: "0.5em" }}>
               {homepageContent.stats.map((stat, index) => (
                 <Grid item xs={6} key={index}>
-                  <Typography variant="h3" fontWeight={600} color="secondary.main">
+                  <Typography
+                    variant="h3"
+                    fontWeight={600}
+                    color="secondary.main"
+                  >
                     {stat.number}
                   </Typography>
                   <Typography variant="body2" color="background.default">
@@ -56,7 +75,10 @@ const StatsSection = () => {
           </Grid>
         </Grid>
       </SlideWrapper>
-      <Container maxWidth={`${isAboveMd ? 'md': 'sm'}`} sx={{ textAlign: "center", my: 8 }}>
+      <Container
+        maxWidth={`${isAboveMd ? "md" : "sm"}`}
+        sx={{ textAlign: "center", my: 8 }}
+      >
         <Typography variant="h3" color="text.primary">
           {homepageContent.statsContentBottomHeading}
         </Typography>
