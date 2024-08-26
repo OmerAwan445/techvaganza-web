2050cf328fac8858066653827308f4bac4550f6d
Hasnat Ahmed Goheer <ch.hsyahmedgoheer@hotmail.com>
2024-08-26 23:34:18 +0500

    Add slide animation to the WhyChooseUsSection

--- src/components/AboutPage/WhyChooseUsSection.tsx
+++ src/components/AboutPage/WhyChooseUsSection.tsx
@@ -1,4 +1,5 @@
 import { Box, Container, Grid, Typography } from "@mui/material";
+import SlideWrapper from "../Wrapper/SlideWrapper";
 
 const WhyChooseUsSection: React.FC<WhyChooseUsSectionProps> = ({
   heading,
@@ -13,71 +14,74 @@ const WhyChooseUsSection: React.FC<WhyChooseUsSectionProps> = ({
         position: "relative",
       }}
     >
-      <Grid container spacing={6} alignItems="center">
-        <Grid item xs={12} md={6}>
-          <Typography
-            variant="h2"
-            color="primary.main"
-            gutterBottom
-            sx={{
-              fontWeight: 600,
-              maxWidth: 700,
-              mx: "auto",
-            }}
+      <SlideWrapper>
+        <Grid container spacing={6} alignItems="center">
+          <Grid item xs={12} md={6}>
+            <Typography
+              variant="h2"
+              color="primary.main"
+              gutterBottom
+              sx={{
+                fontWeight: 600,
+                maxWidth: 700,
+                mx: "auto",
+              }}
+            >
+              {heading}
+            </Typography>
+            <Typography
+              variant="body1"
+              color="text.primary"
+              paragraph
+              sx={{
+                maxWidth: 700,
+                mx: "auto",
+                mb: 6,
+              }}
+            >
+              {description}
+            </Typography>
+          </Grid>
+
+          <Grid
+            item
+            xs={12}
+            md={6}
+            sx={{ position: "relative", display: { xs: "none", md: "block" } }}
           >
-            {heading}
-          </Typography>
-          <Typography
-            variant="body1"
-            color="text.primary"
-            paragraph
-            sx={{
-              maxWidth: 700,
-              mx: "auto",
-              mb: 6,
-            }}
-          >
-            {description}
-          </Typography>
+            <Box
+              sx={{
+                backgroundImage: "url('/images/about/WhyChooseUs-1.jpg')",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                height: "300px",
+                width: "80%",
+                borderRadius: 2,
+                position: "absolute",
+                top: "50%",
+                right: "0",
+                transform: "translateY(-50%) rotate(-5deg)", // Center vertically and rotate slightly
+                zIndex: 1,
+              }}
+            />
+            <Box
+              sx={{
+                backgroundImage: "url('/images/about/WhyChooseUs-2.jpg')",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                height: "300px",
+                width: "80%",
+                borderRadius: 2,
+                position: "absolute",
+                top: "50%",
+                right: "0",
+                transform: "translateY(-50%) rotate(5deg) translateX(20px)", // Center vertically and rotate slightly
+                zIndex: 2,
+              }}
+            />
+          </Grid>
         </Grid>
-        <Grid
-          item
-          xs={12}
-          md={6}
-          sx={{ position: "relative", display: { xs: "none", md: "block" } }}
-        >
-          <Box
-            sx={{
-              backgroundImage: "url('/images/about/WhyChooseUs-1.jpg')",
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              height: "300px",
-              width: "80%",
-              borderRadius: 2,
-              position: "absolute",
-              top: "50%",
-              right: "0",
-              transform: "translateY(-50%) rotate(-5deg)", // Center vertically and rotate slightly
-              zIndex: 1,
-            }}
-          />
-          <Box
-            sx={{
-              backgroundImage: "url('/images/about/WhyChooseUs-2.jpg')",
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              height: "300px",
-              width: "80%",
-              borderRadius: 2,
-              position: "absolute",
-              top: "50%",
-              right: "0",
-              transform: "translateY(-50%) rotate(5deg) translateX(20px)", // Center vertically and rotate slightly
-              zIndex: 2,
-            }}
-          />
-        </Grid>
-      </Grid>
+      </SlideWrapper>
     </Container>
   );
 };
