5683639520560fd3639aabf1f63f05d6d1d462a7
Hasnat Ahmed Goheer <ch.hsyahmedgoheer@hotmail.com>
2024-08-27 21:18:07 +0500

    Change Spelling and fix some issues

--- src/utils/checkFormValidation.ts
+++ src/utils/checkFormValidation.ts
@@ -1,13 +1,18 @@
 import { FormValidationError } from "@/errors/FormValidationError";
 
-function checkFormValidation(data: Omit<ContactusFormData, 'phone' | 'message' | 'last_name'>): void {
+function checkFormValidation(
+  data: Omit<ContactUsFormData, "phone" | "message" | "last_name">
+): void {
   const { first_name, email, service_needed } = data;
 
   // Name validation
   if (!first_name) {
     throw new FormValidationError("First Name is required.", "first_name");
   } else if (first_name.length < 2) {
-    throw new FormValidationError("Name must be at least 2 characters long.", "first_name");
+    throw new FormValidationError(
+      "Name must be at least 2 characters long.",
+      "first_name"
+    );
   }
 
   // Email validation
@@ -15,12 +20,18 @@ function checkFormValidation(data: Omit<ContactusFormData, 'phone' | 'message' |
   if (!email) {
     throw new FormValidationError("Email is required.", "email");
   } else if (!emailPattern.test(email)) {
-    throw new FormValidationError("Please enter a valid email address.", "email");
+    throw new FormValidationError(
+      "Please enter a valid email address.",
+      "email"
+    );
   }
 
   // Service validation
   if (!service_needed) {
-    throw new FormValidationError("Please select Service needed", "service_needed");
+    throw new FormValidationError(
+      "Please select Service needed",
+      "service_needed"
+    );
   }
 }
 
